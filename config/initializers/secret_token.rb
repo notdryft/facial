5c52199748dd9c8288aabd9d8030b55480f8612a
notdryft <gui.corre+github@gmail.com>
2012-11-05 01:34:31 +0000

    Application neuve, sans tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facial::Application.config.secret_token = 'edc94c6e5bd4ae6d37d1b01624453101c3d1bef8077358c5852d63e26acd859505aeae89123305b83c65d3ce07a8cef9f111c572504b50f520ec16b28f5d9c14'
