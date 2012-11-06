cc54524ea9f897e72397df8a533981eb1040c5e0
notdryft <gui.corre+github@gmail.com>
2012-11-06 01:28:22 +0000

    Déplacement des helpers vers l'application

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,21 +1,2 @@
 module LinksHelper
-
-  def yesno_helper(boolean)
-    boolean ? t(:oui) : t(:non)
-  end
-
-  def date_helper(timestamp)
-    formatted_date = l timestamp, :format => '%A %d %B'
-    formatted_time = l timestamp, :format => '%Hh%M'
-
-    date = timestamp.to_date
-    today = Date.today
-    if date == today
-      t 'today', :time => formatted_time
-    elsif today - date == 1
-      t 'yesterday', :time => formatted_time
-    else
-      t 'pastday', :date => formatted_date, :time => formatted_time
-    end
-  end
 end
