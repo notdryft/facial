ba615e98cca8c0c8b346115a79ec767d927e8f0b
notdryft <gui.corre@gmail.com>
2015-03-23 12:00:37 +0100

    Correctly handle coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+unless ENV['CODECLIMATE_REPO_TOKEN'].nil?
+  require 'codeclimate-test-reporter'
+
+  CodeClimate::TestReporter.start
+end
+
 require File.expand_path('../../config/environment', __FILE__)
-
 require 'rails/test_help'
-require 'codeclimate-test-reporter'
-
-CodeClimate::TestReporter.start
 
 class ActiveSupport::TestCase
 
